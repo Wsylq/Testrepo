67a01e72fda4cd99cea80d4d85759aa0daf3e272
Wsylq <sylqw@atomicmail.io>
2026-04-17 17:27:03 +0530

    update DopamineBoxApp.kt

--- android-dopamine-box/app/src/main/java/com/dopaminebox/app/ui/DopamineBoxApp.kt
+++ android-dopamine-box/app/src/main/java/com/dopaminebox/app/ui/DopamineBoxApp.kt
@@ -46,7 +46,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.draw.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
